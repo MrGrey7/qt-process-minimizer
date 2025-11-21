2e7ff10e432e086aac8ccc6ef3a692211b1a15fd
MrGrey <xzwantedxz@gmail.com>
2025-11-21 14:45:32 +0300

    Fixed potential encoding bug

--- main.cpp
+++ main.cpp
@@ -8,7 +8,8 @@ int main(int argc, char *argv[])
     a.setWindowIcon(QIcon(":/icon/web/icon.png"));
     MainWindow w;
     // Check if started minimized
-    if (argc > 1 && QString(argv[1]) == "--minimized") {
+    QStringList args = QCoreApplication::arguments();
+    if (args.size() > 1 && args[1] == "--minimized") {
         w.hide(); // Start hidden, tray icon will be visible
     } else {
         w.show();
